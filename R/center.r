38f0c0db5a0174bae68b0dc55ab8fe5cc178b9f8
ianhussey <Ian@g022h237.guest.ugent.be>
2017-03-17 15:15:12 +0100

    added variable centering function

---
+++ R/center.r
@@ -0,0 +1,25 @@
+#' center variables
+#'
+#' This function allows you to combine multiple ggplot2 plots into one.
+#' Code from https://hlplab.wordpress.com/2009/04/27/centering-several-variables/
+#' @param x Variable or dataframe to be centered.
+#' @keywords 
+#' @export
+#' @examples 
+#' center_vars(my_data)
+
+center_vars = function(x) {
+  if (is.numeric(x)) { return(x - mean(x, na.rm = T)) }
+  if (is.factor(x)) {
+    x = as.numeric(x)
+    return(x - mean(x, na.rm = T))
+  }
+  if (is.data.frame(x) || is.matrix(x)) {
+    m = matrix(nrow = nrow(x), ncol = ncol(x))
+    colnames(m) = paste("c", colnames(x), sep = "")
+    for (i in 1:ncol(x)) {
+      m[,i] = center_vars(x[,i])
+    }
+    return(as.data.frame(m))
+  }
+}
